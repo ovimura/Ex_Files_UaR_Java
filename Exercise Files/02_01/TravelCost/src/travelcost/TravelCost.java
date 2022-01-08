34a03e206faf35e38d4f86779644a83b66c098bf
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-01-08 12:09:26 -0800

    adding java 8 exercices files

--- Exercise Files/02_01/TravelCost/src/travelcost/TravelCost.java
+++ Exercise Files/02_01/TravelCost/src/travelcost/TravelCost.java
@@ -4,10 +4,11 @@
  * and open the template in the editor.
  */
 package travelcost;
+import java.util.Scanner;
 
 /**
  *
- * @author Peggy Fisher
+ * @author Ovidiu Mura
  */
 public class TravelCost {
 
@@ -15,7 +16,22 @@ public class TravelCost {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double distance;
+        double mpg;
+        double pricePerGallon;
+        double totalCost;
+
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter the total distance in miles: ");
+        distance = in.nextDouble();
+        System.out.println("Enter the mpg for the vehicle: ");
+        mpg = in.nextDouble();
+        System.out.println("Enter the price of one gallon of gas: ");
+        pricePerGallon = in.nextDouble();
+
+        totalCost = (distance/mpg)*pricePerGallon;
+        System.out.printf("The trip is going to cost $%5.2f",totalCost);
+        System.out.println();
     }
     
 }
