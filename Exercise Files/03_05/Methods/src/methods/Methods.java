23a0bb24967f52725112ce720f7a3ba3ec0f6d75
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-01-14 12:30:46 -0800

    adding exercices solution for problem of Overloading

--- Exercise Files/03_05/Methods/src/methods/Methods.java
+++ Exercise Files/03_05/Methods/src/methods/Methods.java
@@ -5,6 +5,8 @@
  */
 package methods;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Peggy Fisher
@@ -15,7 +17,19 @@ public class Methods {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        double x, y, z;
+        double avg;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter three numbers: ");
+        x = in.nextDouble();
+        y = in.nextDouble();
+        z = in.nextDouble();
+        avg = average(x,y,z);
+        System.out.println("The average is: "+avg);
     }
-    
+
+    public static double average(double num1, double num2, double num3) {
+        double avg = (num1+num2+num3)/3;
+        return avg;
+    }
 }
--- Exercise Files/03_07/Overloading/src/overloading/Overloading.java
+++ Exercise Files/03_07/Overloading/src/overloading/Overloading.java
@@ -15,7 +15,15 @@ public class Overloading {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("The average of three integers: " + average(3,5,8));
+        System.out.println("The average of three doubles: " + average(3.0,5.0,8.0));
     }
-    
+
+    public static double average(int x, int y, int z) {
+        return (x+y+z)/3.0;
+    }
+
+    public static double average(double x, double y, double z) {
+        return (x+y+z)/3;
+    }
 }
