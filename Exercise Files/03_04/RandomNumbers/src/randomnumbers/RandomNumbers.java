033d8964e86e27d9531fe9a5b34f38217b9584f7
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-01-14 08:35:10 -0800

    adding exercices for Random Numbers

--- Exercise Files/03_04/RandomNumbers/src/randomnumbers/RandomNumbers.java
+++ Exercise Files/03_04/RandomNumbers/src/randomnumbers/RandomNumbers.java
@@ -5,6 +5,8 @@
  */
 package randomnumbers;
 
+import java.util.Random;
+
 /**
  *
  * @author Peggy Fisher
@@ -15,7 +17,15 @@ public class RandomNumbers {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       
+        int die1, die2, countDoubles = 0;
+        Random rand = new Random();
+        for(int i=0; i<100; i++) {
+            die1 = rand.nextInt(6);
+            die2 = rand.nextInt(6);
+            if(die1 == die2)
+                countDoubles++;
+        }
+        System.out.println("I rolled "+countDoubles+" doubles");
     }
     
 }
