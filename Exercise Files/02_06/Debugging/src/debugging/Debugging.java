42448aead01dba14e49bb8d717bb4324628f3392
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-01-13 23:46:56 -0800

    adding exercices java

--- Exercise Files/02_06/Debugging/src/debugging/Debugging.java
+++ Exercise Files/02_06/Debugging/src/debugging/Debugging.java
@@ -19,12 +19,12 @@ public class Debugging {
         String[] daysOfTheWeek = {"Sunday", "Monday", "Tuesday", "Wednesday", 
             "Thursday", "Friday", "Saturday"};
         //sample errors
-        value = 3.75      //example of syntax error, missing semi-colon
-        number = value;   //example of implicit conversion error
-        average = (3 + 5 + 8) / 0;
-        System.out.println("The first day of the week is: "+ daysOfTheWeek[1]);
-        System.out.println("The last day of the week is: "+ daysOfTheWeek[7]);
-        average = 3 + 5 + 8 / 3.0;  //example of logic error 
+        value = 3.75;      //example of syntax error, missing semi-colon
+        number = (int)value;   //example of implicit conversion error
+        // average = (3 + 5 + 8) / 0;
+        System.out.println("The first day of the week is: "+ daysOfTheWeek[0]);
+        System.out.println("The last day of the week is: "+ daysOfTheWeek[6]);
+        average = (3 + 5 + 8) / 3.0;  //example of logic error
         System.out.println("expected value is 5.33, actual value is: " + average);
         }
     }
--- Exercise Files/02_08/SimpleCalculation/src/simplecalculation/SimpleCalculation.java
+++ Exercise Files/02_08/SimpleCalculation/src/simplecalculation/SimpleCalculation.java
@@ -3,6 +3,8 @@
  */
 package simplecalculation;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Peggy Fisher
@@ -13,7 +15,39 @@ public class SimpleCalculation {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        double length, width, height;
+        int num_windows;
+        double size_windows;
+        int num_doors;
+        double size_doors;
+        double total_area;
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Wellcome to Simple Calculation challenge!");
+        System.out.println();
+        System.out.println("Enter the length of the house:");
+        length = in.nextDouble();
+        System.out.println("Enter the width of the house:");
+        width = in.nextDouble();
+        System.out.println("Enter the height of the house:");
+        height = in.nextDouble();
+        System.out.println("Enter the number of the windows:");
+        num_windows = in.nextInt();
+        System.out.println("Enter the size of the windows:");
+        size_windows = in.nextDouble();
+        System.out.println("Enter the number of the doors:");
+        num_doors = in.nextInt();
+        System.out.println("Enter the size of the door:");
+        size_doors = in.nextDouble();
+
+        double sides = 2*(length*height);
+        double front_back = 2*(width*height);
+        double w_area = num_windows*size_windows;
+        double d_area = num_doors*size_doors;
+        total_area = sides + front_back - w_area - d_area;
+
+        System.out.println("\n\nThe total surface are to be painted is:" + total_area);
     }
     
 }
