2398e55321b0f38ea886f20b1133e71f18c1bf12
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-01-14 07:50:49 -0800

    adding problem solution for Calculate Shipping

--- Exercise Files/03_02/CalculateShipping/src/calculateshipping/CalculateShipping.java
+++ Exercise Files/03_02/CalculateShipping/src/calculateshipping/CalculateShipping.java
@@ -8,6 +8,8 @@
  */
 package calculateshipping;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Peggy Fisher
@@ -18,7 +20,17 @@ public class CalculateShipping {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        double totalSale = 0;
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter total for the sale: ");
+        totalSale = in.nextDouble();
+        if(totalSale < 25) {
+            totalSale = totalSale + 15;
+        } else if(totalSale < 50) {
+            totalSale = totalSale + 10;
+        } else if(totalSale <= 75) {
+            totalSale = totalSale + 5;
+        }
+        System.out.println("Total cost for your sale is: " + totalSale);
     }
-    
 }
