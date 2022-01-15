f6b88f67417a5c4b5cfb19debe75964fa9685d28
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-01-15 10:07:37 -0800

    adding movie challenge code

--- Exercise Files/04_06/MovieList/src/movielist/MovieList.java
+++ Exercise Files/04_06/MovieList/src/movielist/MovieList.java
@@ -3,6 +3,8 @@
  */
 package movielist;
 
+import java.util.Scanner;
+
 /**
  *
  * @author Peggy Fisher
@@ -13,7 +15,20 @@ public class MovieList {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner in = new Scanner(System.in);
+        String movie, rating;
+        int year, runtime;
+        Movie favoriteMovie;
+        System.out.println("Please enter movie title: ");
+        movie = in.nextLine();
+        System.out.println("Please enter movie rating: ");
+        rating = in.nextLine();
+        System.out.println("Please enter year produced: ");
+        year = in.nextInt();
+        System.out.println("Please enter total runtime: ");
+        runtime = in.nextInt();
+        favoriteMovie = new Movie(movie, rating, year, runtime);
+        System.out.println(favoriteMovie.toString());
     }
     
 }
