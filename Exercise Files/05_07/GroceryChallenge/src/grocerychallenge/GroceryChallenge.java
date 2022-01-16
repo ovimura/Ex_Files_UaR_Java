11de80d999f1cc5f4691ac87954a473607ee364a
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-01-15 22:52:23 -0800

    adding grocery challenge solutions

--- Exercise Files/05_07/GroceryChallenge/src/grocerychallenge/GroceryChallenge.java
+++ Exercise Files/05_07/GroceryChallenge/src/grocerychallenge/GroceryChallenge.java
@@ -18,7 +18,34 @@ public class GroceryChallenge {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-     
+        Scanner in = new Scanner(System.in);
+        ArrayList<String> groceries = new ArrayList<>();
+        groceries.add("Bananas");
+        groceries.add("Wheat Bread");
+        groceries.add("1% milk");
+        groceries.add("Crackers");
+        groceries.add("Oatmeal");
+        System.out.println("Here is the current list: \n" +
+                groceries.toString());
+        System.out.println("Enter another item or \"quit\" to exit");
+        String newItem = in.nextLine();
+        boolean found = false;
+        while (newItem.equalsIgnoreCase("quit")==false) {
+            found = false;
+            for(int i=0; i<groceries.size(); i++) {
+                if(newItem.equalsIgnoreCase(groceries.get(i))) {
+                    System.out.println("This item is already in the list.");
+                    found = true;
+                }
+            }
+            if(found == false) {
+                groceries.add(newItem);
+            }
+            System.out.println("Enter another item or \"quit\" to exit");
+            newItem = in.nextLine();
+        }
+        System.out.println("\nHere is the final list: \n" +
+                groceries.toString());
     }
     
 }
