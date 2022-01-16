0453cb7e90c14df687e7630376ddcfa9c947d266
ovidiu.mura <ovidiu.mura@pacteraedge.com>
2022-01-15 18:06:16 -0800

    adding solution exercices for ch 05_04

--- Exercise Files/05_03/RealEstateMLS/src/realestatemls/RealEstateMLS.java
+++ Exercise Files/05_03/RealEstateMLS/src/realestatemls/RealEstateMLS.java
@@ -4,6 +4,8 @@
  */
 package realestatemls;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Peggy Fisher
@@ -14,7 +16,14 @@ public class RealEstateMLS {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        ArrayList<Property> mls = new ArrayList<Property>();
+        Property p1 = new Property(10000,"Land",3.5);
+        mls.add(p1);
+        p1 = new Property(230000, "Estate", 2.0, 2, 3);
+        mls.add(p1);
+        System.out.println(mls.toString());
+        mls.remove(1);
+        System.out.println("After: " + mls.toString());
     }
     
 }
